47402688240266ac89d683d5ee70f2709dbd8853
itznayaaa <nhallainecuria@gmail.com>
2026-09-01 01:12:09 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,12 @@
+#escape sequence
+
+# \n
+# \t
+# \r
+# \b
+# \\
+# \"
+
+phrase1 = "\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\tHiii my name is Naya\t\t\t\t**\n\t\t\t\t\t**\t\t\t\tand I'm from BSIT-1B\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**********************************************************************************\n\t\t\t\t\t**********************************************************************************"
+
+print(phrase1)
