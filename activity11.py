1cda45be5d53a6d5b01d79373f3ccede53e753a6
itznayaaa <nhallainecuria@gmail.com>
2026-09-07 11:20:09 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,15 @@
+#import demo
+import getpass #folder
+
+username = "nini"
+password = "nininaya03"
+
+u = input("Input USERNAME ---> ")
+p = getpass.getpass("Input PASSWORD ---> ")
+
+
+if u == username and p == password: 
+	print("ayarnch korikk")
+
+else: 
+	print("muhlee ka")
