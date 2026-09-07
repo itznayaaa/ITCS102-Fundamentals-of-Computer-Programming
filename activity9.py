de0cd56f25d4a15fe31842fdbe434c7e5b845e69
itznayaaa <nhallainecuria@gmail.com>
2026-09-07 10:49:03 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,20 @@
+#relational operator activity 
+
+x = 5
+y = 8
+
+password1 = "sample"
+password2 = "sample"
+
+#1
+print( x > y)
+#2 
+print( x < y)
+#3
+print( x <= y)
+#4
+print( x >= y)
+#5 
+print( x == y)
+print( x== y)
+print(password1 == password2)
