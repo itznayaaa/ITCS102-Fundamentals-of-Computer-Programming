e5f8b6bb6559b02edf7024e0e92d43cfcdefe223
itznayaaa <nhallainecuria@gmail.com>
2026-09-07 11:15:55 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,16 @@
+#basic if else program
+
+username = "nini"
+password = "nininaya03"
+
+u = input("Input USERNAME ---> ")
+p = input("Input PASSWORD ---> ")
+
+
+if u == username and p == password: 
+	print("ayarnch korikk")
+
+else: 
+	print("muhlee ka")
+
+ 
