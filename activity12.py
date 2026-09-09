5ccea42d222e879c52eff7f94a9439bafc7989ff
itznayaaa <nhallainecuria@gmail.com>
2026-09-09 11:07:09 +0800

    Update activity12.py

--- activity12.py
+++ activity12.py
@@ -8,6 +8,15 @@ name = input("Please input your name ---> ")
 age = int(input("Please input your age ----> "))
 
 
+if age >= 0 and age <=5 : 
+	print("That age is considered as INFANT ")
+
+elif age >= 0 and age <=5 : 
+	print("That age is considered as INFANT ")
+
+else: 
+	print("age invalid")
+
 if age >= 6 and age <=12 : 
 	print("That age is considered as KID ")
 
@@ -16,4 +25,32 @@ elif age >= 6 and age <=12 :
 
 else: 
 	print("age invalid")
+
+if age >=13 and age <=16 : 
+	print("That age is considered as PRE-TEENS ")
+
+elif age >=13 and age <=16 : 
+	print("That age is considered as PRE-TEENS")
+
+else: 
+	print("age invalid")
+
+if age >= 16 and age <=19 : 
+	print("That age is considered as TEENAGER")
+
+elif age >= 16 and age <=19 : 
+	print("That age is considered as TEENAGER ")
+
+else: 
+	print("age invalid")
+
+if age >= 20 and age <=29 : 
+	print("That age is considered as EARLY ADULT ")
+
+elif age >= 20 and age <=29 : 
+	print("That age is considered as EARLY ADULT ")
+
+else: 
+	print("age invalid")
+
 
