ae4ff0b2652fb17ae98e7413bb162c6c59b97710
itznayaaa <nhallainecuria@gmail.com>
2026-09-09 21:13:02 +0800

    Create activity7.py

---
+++ activity7.py
@@ -0,0 +1,23 @@
+total = 8
+
+
+n = eval(input("enter any number --> "))
+total = total + n
+
+a = eval(input("enter any number --> "))
+total += a
+
+e = eval(input("enter any number --> "))
+total += e
+
+i = eval(input("enter any number --> "))
+total += i
+
+o = eval(input("enter any number --> "))
+total -= o
+
+u = eval(input("enter any number --> "))
+total *= u
+
+
+print("Current total is ---> ", total)
