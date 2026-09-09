240519dee619127d3743b0a6387433942fa28a72
itznayaaa <nhallainecuria@gmail.com>
2026-09-09 21:14:18 +0800

    Create activity8.py

---
+++ activity8.py
@@ -0,0 +1,24 @@
+hobbies = "" #empty string
+
+
+h = input("What are your hobbies? --> ")
+hobbies += h + ", "
+
+h = input("What else? --> ")
+hobbies += h + ", "
+
+h = input("What else? --> ")
+hobbies += h + ", "
+
+h = input("What else? --> ")
+hobbies += h + ", "
+
+h = input("What else? --> ")
+hobbies += h + " and "
+
+h = input("What else? --> ")
+hobbies += h + ". "
+
+
+
+print("My hobbies are {", hobbies ,"}")
