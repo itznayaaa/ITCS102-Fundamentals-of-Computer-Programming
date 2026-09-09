9842b233895e8f291f84280d25ca01b3030dda1a
itznayaaa <nhallainecuria@gmail.com>
2026-09-09 10:58:42 +0800

    Add files via upload

---
+++ activity12.py
@@ -0,0 +1,19 @@
+#multiple if and elif conditions
+
+#Create a python program that would capture age group
+
+
+name = input("Please input your name ---> ")
+
+age = int(input("Please input your age ----> "))
+
+
+if age >= 6 and age <=12 : 
+	print("That age is considered as KID ")
+
+elif age >= 6 and age <=12 : 
+	print("That age is considered as KID")
+
+else: 
+	print("age invalid")
+
